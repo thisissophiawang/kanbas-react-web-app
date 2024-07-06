f0d8579e425ea96d7a456a673bbad0a663bb3260
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 13:21:28 -0700

    Added Tables component and updated Lab1 component

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,7 @@
 import Paragraphs from "./Paragraphs";
 import Headings from "./Headings";
 import Lists from "./Lists";
+import Tables from "./Tables";
 
 export default function Lab1() {
   return (
@@ -11,45 +12,7 @@ export default function Lab1() {
       <Headings />
       <Paragraphs />
       <Lists />
-      <div id="wd-tables">
-        <h4>Table Tag</h4>
-        <table border={1} width="100%">
-          <thead>
-            <tr>
-              <th>Quiz</th>
-              <th>Topic</th>
-              <th>Date</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Q1</td>
-              <td>HTML</td>
-              <td>2/3/21</td>
-              <td>85</td>
-            </tr>
-            <tr>
-              <td>Q2</td>
-              <td>CSS</td>
-              <td>2/10/21</td>
-              <td>90</td>
-            </tr>
-            <tr>
-              <td>Q3</td>
-              <td>JavaScript</td>
-              <td>2/17/21</td>
-              <td>95</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan={3}>Average</td>
-              <td>90</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+      <Tables />
     </div>
   );
 }
