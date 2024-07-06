61669a3c2aadc11446c98bff8f78528d4f3d8228
Xinyi <xinyiwang2013@gmail.com>
2024-07-05 23:24:46 -0700

    Updated App component with correct anchor tag for GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Labs from "./Labs";
 import logo from "./logo.svg";
 
-
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Web Dev</h1>
       <h2>Name: Xinyi Wang/Sophia</h2>
       <Labs />
+      {/* Kanbas */}
+      <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">
+        Github
+      </a>
     </div>
   );
 }
+
 export default App;
