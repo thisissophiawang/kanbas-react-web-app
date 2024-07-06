61931b5e97cafa5213902478bbe4b182dd2ac427
Xinyi <xinyiwang2013@gmail.com>
2024-07-05 18:10:22 -0700

    Added full name to Labs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Labs from "./Labs";
+import logo from "./logo.svg";
+
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Web Dev</h1>
-      <h2>Xinyi Wang/Sophia</h2>
+      <h2>Name: Xinyi Wang/Sophia</h2>
       <Labs />
     </div>
   );
