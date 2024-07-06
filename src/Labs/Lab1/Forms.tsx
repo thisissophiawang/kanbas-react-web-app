99187c5d4dd66c0a7a6fa277ad9e0aa79b7debd5
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 13:51:43 -0700

    Added Forms, Images, and Tables components; updated Lab1 component

---
+++ src/Labs/Lab1/Forms.tsx
@@ -0,0 +1,21 @@
+export default function Forms(){
+    return (
+        <div id="wd-forms">
+  <h4>Form Elements</h4>
+  <form id="wd-text-fields">
+    <h5>Text Fields</h5>
+    <label htmlFor="wd-text-fields-username">Username:</label>
+    <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+    <label htmlFor="wd-text-fields-password">Password:</label>
+    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+    <br />
+    <label htmlFor="wd-text-fields-first-name">First name:</label>
+    <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+      value="Wonderland" title="The last name" />
+    {/* copy rest of form elements here  */}
+  </form>
+</div>
+    );
+}
