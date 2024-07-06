3226b4ce6141e69202267e01301ccb19cd2fa3a7
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 13:10:10 -0700

    Added Lists component and updated Lab1 component

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,43 +1,17 @@
+
+import Paragraphs from "./Paragraphs";
+import Headings from "./Headings";
+import Lists from "./Lists";
+
 export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
-      <div id="wd-h-tag">
-        <h4>Heading Tags</h4>
-        Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance, this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
-      </div>
-      <div id="wd-p-tag">
-        <h4>Paragraph Tag</h4>
-        <p id="wd-p-1">
-          This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, wrap the paragraphs you want to separate with the paragraph tag.
-        </p>
-      </div>
-      <div id="wd-lists">
-        <h4>List Tags</h4>
-        <h5>Ordered List Tag</h5>
-        How to make pancakes:
-        <ol>
-          <li>Mix dry ingredients.</li>
-          <li>Add wet ingredients.</li>
-          <li>Stir to combine.</li>
-          <li>Heat a skillet or griddle.</li>
-          <li>Pour batter onto the skillet.</li>
-          <li>Cook until bubbly on top.</li>
-          <li>Flip and cook the other side.</li>
-          <li>Serve and enjoy!</li>
-        </ol>
-        <h5>Unordered List Tag</h5>
-        My favorite books (in no particular order)
-        <ul id="wd-my-books">
-        <li>Dune</li>
-        <li>Lord of the Rings</li>
-        <li>Ender's Game</li>
-        <li>Red Mars</li>
-        <li>The Forever War</li>
-        </ul>
-     </div>
-     <div id="wd-tables">
+      <Headings />
+      <Paragraphs />
+      <Lists />
+      <div id="wd-tables">
         <h4>Table Tag</h4>
         <table border={1} width="100%">
           <thead>
@@ -79,6 +53,3 @@ export default function Lab1() {
     </div>
   );
 }
-
-
-  
