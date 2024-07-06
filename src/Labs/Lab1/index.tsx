584f3225ee462895b6300ea6a4297c2798070cf6
Xinyi <xinyiwang2013@gmail.com>
2024-07-05 18:25:12 -0700

    commit on lab1,2,3

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,8 +1,9 @@
 export default function Lab1() {
-    return (
-      <div id="wd-lab1">
-        <h2>Lab 1</h2>
-      </div>
-    );
-  }
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
+    </div>
+  );
+}
+
   
