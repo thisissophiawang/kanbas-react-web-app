442cbfa16e788a0f12e883b3971fb9e7b2a84e6b
Xinyi <xinyiwang2013@gmail.com>
2024-07-07 09:28:38 -0700

    Updated course names in Dashboard component

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,25 +1,48 @@
 export default function Dashboard() {
-    return (
-      <div id="wd-dashboard">
-        <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-        <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-        <div id="wd-dashboard-courses">
-          <div className="wd-dashboard-course">
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <a className="wd-dashboard-course-link"
-                href="#/Kanbas/Courses/1234/Home">
-                CS1234 React JS
-              </a>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
-            </div>
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2>
+      <hr />
+      <div id="wd-dashboard-courses">
+        <div className="wd-dashboard-course">
+          <img src="/images/reactjs.jpg" width={200} alt="React JS" />
+          <div>
+            <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/1234/Home">
+              CS1234 React JS
+            </a>
+            <p className="wd-dashboard-course-title">
+              Full Stack software developer
+            </p>
+            <a href="#/Kanbas/Courses/1234/Home"> Go </a>
           </div>
-          <div className="wd-dashboard-course"> ... </div>
-          <div className="wd-dashboard-course"> ... </div>
+        </div>
+        <div className="wd-dashboard-course">
+          <img src="/images/reactjs.jpg" width={200} alt="React JS" />
+          <div>
+            <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/2345/Home">
+              CS2345 Angular
+            </a>
+            <p className="wd-dashboard-course-title">
+              Full Stack software developer
+            </p>
+            <a href="#/Kanbas/Courses/2345/Home"> Go </a>
+          </div>
+        </div>
+        <div className="wd-dashboard-course">
+          <img src="/images/reactjs.jpg" width={200} alt="React JS" />
+          <div>
+            <a className="wd-dashboard-course-link" href="#/Kanbas/Courses/4321/Home">
+              CS4321 View.js
+            </a>
+            <p className="wd-dashboard-course-title">
+              Full Stack software developer
+            </p>
+            <a href="#/Kanbas/Courses/4321/Home"> Go </a>
+          </div>
         </div>
       </div>
-  );}
-  
+    </div>
+  );
+}
