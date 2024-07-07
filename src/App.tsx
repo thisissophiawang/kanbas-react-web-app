ece009b29236eeb99d1eb873c9b3f7bf249dd510
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 22:59:56 -0700

    Added TOC component and updated routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,9 @@ function App() {
   return (
     <HashRouter>
       <div>
-        <h1>Welcome to Web Dev</h1>
-        <h2>Name: Xinyi Wang/Sophia</h2>
+        <h1>Xinyi Wang/Sophia</h1>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="Labs/*" element={<Labs />} />
+          <Route path="/Labs" element={<Labs />} />
         </Routes>
         {/* Kanbas */}
         <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">
---
+++ src/Labs/TOC.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TOC() {
+  return (
+    <ul>
+      <li id="wd-a"><a href="#/Labs">Labs</a></li>
+      <li id="wd-a1"><a href="#/Labs/Lab1">Lab 1</a></li>
+      <li id="wd-a2"><a href="#/Labs/Lab2">Lab 2</a></li>
+      <li id="wd-a3"><a href="#/Labs/Lab3">Lab 3</a></li>
+    </ul>
+  );
+}
