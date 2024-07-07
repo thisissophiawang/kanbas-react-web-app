02a145bb4b4da6fe0649668e7d93080dd99482d6
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 23:45:22 -0700

    Deleted Labs.tsx and updated TOC and index.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 export default function TOC() {
   return (
     <ul>
-      <li id="wd-a"><a href="#/Labs">Labs</a></li>
-      <li id="wd-a1"><a href="#/Labs/Lab1">Lab 1</a></li>
-      <li id="wd-a2"><a href="#/Labs/Lab2">Lab 2</a></li>
-      <li id="wd-a3"><a href="#/Labs/Lab3">Lab 3</a></li>
+      <li><a id="wd-a"  href="#/Labs">Labs</a></li>
+      <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
+      <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
+      <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
     </ul>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Lab1 from "./Lab1";
+import { Route, Routes, Navigate } from "react-router";
+import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import TOC from "./TOC";
-
 
 export default function Labs() {
   return (
@@ -12,12 +10,11 @@ export default function Labs() {
       <h1>Labs</h1>
       <TOC />
       <Routes>
-        <Route path="/Lab1" element={<Lab1 />} />
-        <Route path="/Lab2" element={<Lab2 />} />
-        <Route path="/Lab3" element={<Lab3 />} />
+        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Lab3" element={<Lab3 />} />
       </Routes>
     </div>
   );
 }
-
-
