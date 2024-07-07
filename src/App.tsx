4337173ff2bd6897c10c6c3f317f8fe1378882fb
Xinyi <xinyiwang2013@gmail.com>
2024-07-06 19:45:23 -0700

    Updated App component with HashRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Web Dev</h1>
-      <h2>Name: Xinyi Wang/Sophia</h2>
-      <Labs />
-      {/* Kanbas */}
-      <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">
-        Github
-      </a>
-    </div>
+    <HashRouter>
+      <div>
+        <h1>Welcome to Web Dev</h1>
+        <h2>Name: Xinyi Wang/Sophia</h2>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="Labs/*" element={<Labs />} />
+        </Routes>
+        {/* Kanbas */}
+        <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">
+          Github
+        </a>
+      </div>
+    </HashRouter>
   );
 }
 
