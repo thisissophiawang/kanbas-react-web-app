b3b8df5f3a712b17101e716a9323139f77da9c57
Xinyi <xinyiwang2013@gmail.com>
2024-07-09 14:04:46 -0700

    Add LandingPage component and update links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Kanbas from "./Kanbas";
+import LandingPage from "./Kanbas/Landing Page";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/Kanbas" />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/Kanbas" element={<LandingPage />} />
         </Routes>
         <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github</a>
       </div>
