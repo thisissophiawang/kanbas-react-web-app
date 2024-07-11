24f94bb5232773c2b2f423862b6fa4b214ef4ae7
Xinyi <xinyiwang2013@gmail.com>
2024-07-10 17:36:25 -0700

    update the lab2 selecting contenet based on the document structure.

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -15,11 +15,11 @@ const Lab2: React.FC = () => {
       <div id="wd-css-id-selectors">
         <h3>ID selectors</h3>
         <p id="wd-id-selector-1">
-Instead of changing the look and feel of all the 
-elements of the same name, e.g., P, we can refer to a specific element by its ID
+    Instead of changing the look and feel of all the 
+    elements of the same name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-Here's another paragraph using a different ID and a different look and
+    Here's another paragraph using a different ID and a different look and
           feel
         </p>
       </div>
@@ -31,6 +31,26 @@ Instead of using IDs to refer to elements, you can use an element's CLASS attrib
   <h4 className="wd-class-selector">
 This heading has same style as paragraph above
   </h4>
+</div>
+<div id="wd-css-document-structure">
+  <div className="wd-selector-1">
+    <h3>Document structure selectors</h3>
+    <div className="wd-selector-2">
+      Selectors can be combined to refer elements in particular
+      places in the document
+      <p className="wd-selector-3">
+        This paragraph's red background is referenced as
+        <br />
+        .selector-2 .selector3<br />
+        meaning the descendant of some ancestor.<br />
+        <span className="wd-selector-4">
+          Whereas this span is a direct child of its parent
+        </span><br />
+          You can combine these relationships to create specific 
+          styles depending on the document structure
+      </p>
+    </div>
+  </div>
 </div>
 
     </div>
