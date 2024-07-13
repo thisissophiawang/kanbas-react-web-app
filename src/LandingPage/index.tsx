5963df903bea3772c182d6b3496afb4214f64691
Xinyi <xinyiwang2013@gmail.com>
2024-07-13 17:02:43 -0500

    edit the landing page to add Kanbas link and remove the a1 hw links.

--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -5,6 +5,8 @@ const LandingPage: React.FC = () => {
   return (
     <div>
       <h1>Landing Page</h1>
+      <br></br>
+      <h2>Welcome to CS5610 Web Development </h2>
       <p><strong>Name:</strong> Xinyi Wang/Sophia</p>
       <p><strong>Section:</strong> CS5610 Summer II</p>
 
@@ -13,6 +15,11 @@ const LandingPage: React.FC = () => {
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+      </ul>
+
+      <h2>Kanbas</h2>
+      <ul>
+        <li><Link to="/Kanbas">Kanbas</Link></li>
       </ul>
 
       <h2>Links to all relevant source code repositories</h2>
