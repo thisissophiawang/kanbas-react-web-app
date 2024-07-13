f295495fd9dbe52055dd6c42e83a80d89a3328f0
Xinyi <xinyiwang2013@gmail.com>
2024-07-13 16:46:35 -0500

    Updated LandingPage links to direct to Labs components

--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -8,29 +8,18 @@ const LandingPage: React.FC = () => {
       <p><strong>Name:</strong> Xinyi Wang/Sophia</p>
       <p><strong>Section:</strong> CS5610 Summer II</p>
 
-      <h2>A1 Links</h2>
-      <ul>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Account">Account</a></li>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Dashboard">Dashboard</a></li>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Courses/1234/Home">Course</a></li>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Courses/1234/Modules">Modules</a></li>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Courses/1234/Assignments">Assignments</a></li>
-        <li><a href="https://a1--kanbas-react-web-app-sophia.netlify.app/#/Kanbas/Courses/1234/Assignments/123">Assignment Edit</a></li>
-      </ul>
-
-      <h2>Links to all relevant source code repositories</h2>
-      <p><a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github Repository</a></p>
-
       <h2>Labs</h2>
       <ul>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       </ul>
+
+      <h2>Links to all relevant source code repositories</h2>
+      <p><a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github Repository</a></p>
+
     </div>
   );
 }
 
 export default LandingPage;
-
-
