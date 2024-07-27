19476a54e1ca61e2e8bea238651b44a4f0bad841
Xinyi <xinyiwang2013@gmail.com>
2024-07-27 15:28:46 -0700

    add passing data on event.

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,11 +1,13 @@
 ///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/Lab4/index.tsx
 import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 
 export default function Lab4() {
     return (
         <div id="wd-a4">
             <h2>Lab 4</h2>
             <ClickEvent />
+            <PassingDataOnEvent />
         </div>
     );
 }
