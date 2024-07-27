3df1632667ccca24089016631e8caa17b3276ecf
Xinyi <xinyiwang2013@gmail.com>
2024-07-27 15:34:49 -0700

    add passing function.

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,13 +1,19 @@
 ///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/Lab4/index.tsx
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
 
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+      }
+      
     return (
         <div id="wd-a4">
             <h2>Lab 4</h2>
             <ClickEvent />
             <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello}/>
         </div>
     );
 }
