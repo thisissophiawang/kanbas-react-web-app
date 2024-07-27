2bc61056019577ec6db370337585a6d6b172a010
Xinyi <xinyiwang2013@gmail.com>
2024-07-27 15:24:06 -0700

    add click event.

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,9 +1,11 @@
 ///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/Lab4/index.tsx
+import ClickEvent from "./ClickEvent";
 
 export default function Lab4() {
     return (
         <div id="wd-a4">
             <h2>Lab 4</h2>
+            <ClickEvent />
         </div>
     );
 }
