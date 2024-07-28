8f06f0a1da775d4bfc0cb815545f26d140b1604d
Xinyi <xinyiwang2013@gmail.com>
2024-07-27 18:36:03 -0700

    modify the to do list format.

--- src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -1,23 +1,27 @@
-///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addTodo, updateTodo, setTodo } from "./todosReducer";
 
-export default function TodoForm(
-) {
+export default function TodoForm() {
   const { todo } = useSelector((state: any) => state.todosReducer);
   const dispatch = useDispatch();
   return (
     <li className="list-group-item">
-      <button onClick={() => dispatch(addTodo(todo))}
-              id="wd-add-todo-click"> Add </button>
-      <button onClick={() => dispatch(updateTodo(todo))}
-              id="wd-update-todo-click"> Update </button>
-      <input
-        value={todo.title}
-        onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))}
-      />
+      <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+        <button onClick={() => dispatch(addTodo(todo))} id="wd-add-todo-click">
+          Add
+        </button>
+        <button onClick={() => dispatch(updateTodo(todo))} id="wd-update-todo-click">
+          Update
+        </button>
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <input
+          value={todo.title}
+          onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))}
+          style={{ width: '100%', padding: '5px', boxSizing: 'border-box' }}
+        />
+      </div>
     </li>
   );
 }
--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,27 +1,19 @@
-///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
-
 import { deleteTodo, setTodo } from "./todosReducer";
 import { useDispatch } from "react-redux";
-export default function TodoItem({
-  todo,
-}: {
-  todo: { id: string; title: string };
-}) {
+
+export default function TodoItem({ todo }: { todo: { id: string; title: string } }) {
   const dispatch = useDispatch();
   return (
     <li key={todo.id} className="list-group-item">
-      <button
-        onClick={() => dispatch(deleteTodo(todo.id))}
-        id="wd-delete-todo-click"
-      >
-        {" "}
-        Delete{" "}
-      </button>
-      <button onClick={() => dispatch(setTodo(todo))} id="wd-set-todo-click">
-        {" "}
-        Edit{" "}
-      </button>
-      {todo.title}
+      <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+        <button onClick={() => dispatch(deleteTodo(todo.id))} id="wd-delete-todo-click">
+          Delete
+        </button>
+        <button onClick={() => dispatch(setTodo(todo))} id="wd-set-todo-click">
+          Edit
+        </button>
+        <span>{todo.title}</span>
+      </div>
     </li>
   );
 }
