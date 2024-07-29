d42cd9de8ffbdb18b7450ab70fe1e4e5b493b237
Xinyi <xinyiwang2013@gmail.com>
2024-07-28 23:06:28 -0700

    Creating reducer.ts for assignment.

---
+++ src/Kanbas/Courses/Assignments/reducer.ts
@@ -0,0 +1,32 @@
+// src/Kanbas/Courses/Assignments/reducer.ts
+import { createSlice } from '@reduxjs/toolkit';
+import { assignments as dbAssignments } from '../../Database';
+
+const initialState = {
+  assignments: dbAssignments,
+};
+
+const assignmentsSlice = createSlice({
+  name: 'assignments',
+  initialState,
+  reducers: {
+    addAssignment: (state, { payload: assignment }) => {
+      const newAssignment: any = {
+        _id: new Date().getTime().toString(),
+        ...assignment,
+      };
+      state.assignments = [...state.assignments, newAssignment];
+    },
+    deleteAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.filter((a) => a._id !== assignmentId);
+    },
+    updateAssignment: (state, { payload: assignment }) => {
+      state.assignments = state.assignments.map((a) =>
+        a._id === assignment._id ? assignment : a
+      );
+    },
+  },
+});
+
+export const { addAssignment, deleteAssignment, updateAssignment } = assignmentsSlice.actions;
+export default assignmentsSlice.reducer;
