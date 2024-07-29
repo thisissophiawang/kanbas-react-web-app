3448b9576703ab5a93f31054a7528b03418c6c5a
Xinyi <xinyiwang2013@gmail.com>
2024-07-28 23:07:18 -0700

    Adding the Assignments Reducer to the Store.

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,10 +1,12 @@
 // src/Kanbas/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import modulesReducer from './Courses/Modules/reducer';
+import assignmentsReducer from './Courses/Assignments/reducer'; // Import the assignments reducer
 
 const store = configureStore({
   reducer: {
     modules: modulesReducer,
+    assignments: assignmentsReducer, // Add the assignments reducer
   },
 });
 
