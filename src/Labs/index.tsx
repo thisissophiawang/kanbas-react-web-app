d7569537306dbdfd7e85856e36c309a79c80d9f8
Xinyi <xinyiwang2013@gmail.com>
2024-08-03 17:56:00 -0700

    add route for lab5 in index file.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,7 @@ import Lab1 from './Lab1';
 import Lab2 from './Lab2';
 import Lab3 from './Lab3';
 import Lab4 from './Lab4';
+import Lab5 from './Lab5';
 import TOC from './TOC';
 import store from './store';
 import { Provider } from 'react-redux';
@@ -23,6 +24,7 @@ const Labs: React.FC = () => {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
         <Route path="Lab4/*" element={<Lab4 />} />
+        <Route path="Lab5/*" element={<Lab5 />} />
       </Routes>
       <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github</a>
     </div>
