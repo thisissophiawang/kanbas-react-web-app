fd9a770c902b3a6c2eb7e498fa3095033e16a3c2
Xinyi <xinyiwang2013@gmail.com>
2024-08-10 21:49:35 -0700

    create people's table for courses.

---
+++ src/Kanbas/Courses/People/Table.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import * as client from "./client";
+export default function PeopleTable() {
+  const [users, setUsers] = useState<any[]>([]);
+  const fetchUsers = async () => {
+    const users = await client.findAllUsers();
+    setUsers(users);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  return (
+    <div id="wd-people-table">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+<th>Name</th>
+<th>Login ID</th>
+<th>Section</th>
+<th>Role</th>
+<th>Last Activity</th>
+<th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <span className="wd-first-name">{user.firstName}</span>
+                <span className="wd-last-name">{user.lastName}</span>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
