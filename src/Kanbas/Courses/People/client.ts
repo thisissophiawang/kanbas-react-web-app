d90b5c296b159bf43bde9668d471c7238b6be22b
Xinyi <xinyiwang2013@gmail.com>
2024-08-11 00:20:02 -0700

    Find Users By Partial Name Route

--- src/Kanbas/Courses/People/client.ts
+++ src/Kanbas/Courses/People/client.ts
@@ -10,3 +10,9 @@ export const findUsersByRole= async (role: string) => {
   const response = await axios.get(`${USERS_API}?role=${role}`);
   return response.data;
 };
+export const findUsersByPartialName
+  = async (name: string) => {
+    const response = await
+      axios.get(`${USERS_API}?name=${name}`);
+    return response.data;
+};
