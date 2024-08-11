a9ea251ce7ba60cfdaf96d2954acb276235a56d7
Xinyi <xinyiwang2013@gmail.com>
2024-08-11 14:26:52 -0700

    Implement an Account component with a default navigation route to the Sign in screen

---
+++ src/Kanbas/Courses/Account/index.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Signin from "./Signin";
+import AccountNavigation from "./Navigation";
+import { useSelector } from "react-redux";
+export default function Account() {
+  return (
+    <div className="wd-account-screen">
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <AccountNavigation />
+        </div>
+        <div className="flex-fill p-4 pt-0">
+          <Routes>
+            <Route path="/" element={ <Navigate to="/Kanbas/Account/Signin" /> } />
+            <Route path="/Signin" element={<Signin />} />
+            <Route path="/Signup" element={<h1>Sign Up</h1>} />
+            <Route path="/Profile" element={<h1>Profile</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
