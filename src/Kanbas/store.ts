b6d8cc477a2ee348ea8da64e634ac3b81c92fc74
Xinyi <xinyiwang2013@gmail.com>
2024-08-12 00:10:01 -0700

    Add the new reducer to the store

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -2,11 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modulesReducer from './Courses/Modules/reducer';
 import assignmentsReducer from './Courses/Assignments/reducer'; // Import the assignments reducer
+import accountReducer from './Courses/Account/reducer'; // Import the account reducer
+
 
 const store = configureStore({
   reducer: {
     modulesReducer,
     assignmentsReducer, // Add the assignments reducer
+    accountReducer, // Add the account reducer
   },
 });
 
