8ee9abf329a01c68d4aec2b11c1f857f6c998e5f
Xinyi <xinyiwang2013@gmail.com>
2024-08-11 22:47:58 -0700

    The Web API route for signing in responds with an error if the user is not found. The error can be caught in the Signin component by wrapping the request with a try catch block as shown below. If there's an error, the component will not navigate to the Profile, and instead display the error as an alert box.

--- src/Kanbas/Courses/Account/Signin.tsx
+++ src/Kanbas/Courses/Account/Signin.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as client from "./client";
+
 export default function Signin() {
   const [credentials, setCredentials] = useState<any>({});
+  const [error, setError] = useState("");  // State to handle error messages
   const navigate = useNavigate();
   const signin = async () => {
+    try {
     await client.signin(credentials);
     navigate("/Kanbas/Account/Profile");
+  } catch (err: any) {
+    setError(err.response.data.message);
+  }
   };
+
   return (
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
+      {error && <div className="wd-error alert alert-danger">{error}</div>}
       <input id="wd-username" onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
         value={credentials.username} className="form-control mb-2" placeholder="username" />
       <input id="wd-password" onChange={(e) => setCredentials({ ...credentials, password: e.target.value }) }
