9769f0e22d8027bd8c3ef848d4899736744c3aa1
Xinyi <xinyiwang2013@gmail.com>
2024-08-12 01:39:11 -0700

    when some one is signed in, and they navigate to the Account, the user should automatically navigate to their Profile.

--- src/Kanbas/Courses/Account/index.tsx
+++ src/Kanbas/Courses/Account/index.tsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import Signup from "./Signup";
 
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div className="wd-account-screen">
       <div className="d-flex">
@@ -15,7 +16,7 @@ export default function Account() {
         </div>
         <div className="flex-fill p-4 pt-0">
           <Routes>
-            <Route path="/" element={ <Navigate to="/Kanbas/Account/Signin" /> } />
+            <Route path="/" element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" }/>}/>
             <Route path="/Signin" element={<Signin />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Profile" element={<Profile />} />
