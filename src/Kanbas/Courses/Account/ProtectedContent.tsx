ccc96f61c251500aa26eb5dcc451ce6da02c33dc
Xinyi <xinyiwang2013@gmail.com>
2024-08-12 19:26:49 -0700

    fix the error

--- src/Kanbas/Courses/Account/ProtectedContent.tsx
+++ src/Kanbas/Courses/Account/ProtectedContent.tsx
@@ -1,4 +1,4 @@
-//protected content
+//protected content component to show content only if user is logged in
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
