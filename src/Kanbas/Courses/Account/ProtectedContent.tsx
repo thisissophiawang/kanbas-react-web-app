ebea9edb8f17447ff3331b48171f726825056df1
Xinyi <xinyiwang2013@gmail.com>
2024-08-12 19:55:59 -0700

    add docstring

--- src/Kanbas/Courses/Account/ProtectedContent.tsx
+++ src/Kanbas/Courses/Account/ProtectedContent.tsx
@@ -1,4 +1,5 @@
 //protected content component to show content only if user is logged in
+//if user is not logged in, it will return null
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
